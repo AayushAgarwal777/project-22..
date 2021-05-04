444602c5ae87946b89cbaf197d080b8f31ecf30d
AayushAgarwal777 <73937225+AayushAgarwal777@users.noreply.github.com>
2021-05-04 13:05:29 +0530

    Add files via upload

---
+++ log.js
@@ -0,0 +1,29 @@
+class Log {
+    constructor(x, y,width, height) {
+      var options = {
+          'restitution':0.1,
+          'friction':1.5,
+          'density':1.0,
+          isStatic:true
+      }
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width =width;
+      this.height = height;
+    
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      strokeWeight(5);
+      stroke("green");
+      fill(255);
+      rect(0, 0, this.width, this.height);
+      pop();
+    }
+  };
+  
